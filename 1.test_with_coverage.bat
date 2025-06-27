685bd93012292658be4b824a95e0e4b0715af165
Kyungsoo <ksna703@gmail.com>
2025-06-28 00:23:48 +0900

    [Modi] coverage 설정 변겅

--- 1.test_with_coverage.bat
+++ 1.test_with_coverage.bat
@@ -1,9 +1,20 @@
 @echo off
 
+REM 가상환경 활성화
 call C:\Users\Kyungsoo\anaconda3\Scripts\activate.bat py310
 
+REM 프로젝트 루트 디렉터리로 이동
 cd /d C:\Users\Kyungsoo\Documents\Code\Investment
 
-pytest
+REM 테스트 실행 + 커버리지 리포트 출력 (HTML + 터미널 컬러)
+pytest --color=yes ^
+       --cov=brokers ^
+       --cov=api ^
+       --cov=services ^
+       --cov=core ^
+       --cov-report=term-missing ^
+       --cov-report=html ^
+       --cov-config=.coveragerc
 
+REM HTML 커버리지 리포트 자동 열기
 start htmlcov\index.html
