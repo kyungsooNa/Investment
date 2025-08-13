b54ade7a0e04738e4df670819291f81f849b417c
Kyungsoo <ksna703@gmail.com>
2025-08-13 22:39:45 +0900

    [Modi] sleep 기능 일원화하여 tc에서 조절할 수 있도록 수정

--- tests/unit_test/test_korea_invest_account_api.py
+++ tests/unit_test/test_korea_invest_account_api.py
@@ -2,70 +2,52 @@ import pytest
 from unittest.mock import AsyncMock, MagicMock
 
 from brokers.korea_investment.korea_invest_account_api import KoreaInvestApiAccount
+from tests.unit_test.test_time_manager import time_manager
+
+
+def get_api():
+    mock_logger = MagicMock()
+    mock_env = MagicMock()
+    mock_time_manager = AsyncMock()
+
+    return  KoreaInvestApiAccount(
+        env=mock_env,
+        logger=mock_logger,
+        time_manager=mock_time_manager,
+    )
 
 @pytest.mark.asyncio
 async def test_get_account_balance():
-    # Mock config
-    mock_config = {
-        'tr_ids': {'account': {'inquire_balance_paper': 'mock_tr_id'}},
-        'custtype': 'P',
-        'stock_account_number': '12345678'
-    }
+    api = get_api()
+    api._env.is_paper_trading = True
 
-    mock_logger = MagicMock()
-    mock_env = MagicMock()
-    mock_env.is_paper_trading = True
-    mock_api = KoreaInvestApiAccount(
-        env=mock_env,
-        logger=mock_logger
-    )
-    mock_api.call_api = AsyncMock(return_value={'result': 'success'})
+    api.call_api = AsyncMock(return_value={'result': 'success'})
 
-    result = await mock_api.get_account_balance()
+    result = await api.get_account_balance()
 
     assert result == {'result': 'success'}
-    mock_logger.info.assert_called()
+    api._logger.info.assert_called()
 
 @pytest.mark.asyncio
 async def test_get_real_account_balance_with_dash():
-    mock_config = {
-        'tr_ids': {'account': {'inquire_balance_real': 'real_tr_id'}},
-        'custtype': 'P',
-        'stock_account_number': '12345678-01'
-    }
+    api = get_api()
+    api._env.is_paper_trading = False
 
-    mock_env = MagicMock()
-    mock_env.is_paper_trading = False
-    mock_logger = MagicMock()
-    mock_api = KoreaInvestApiAccount(
-        env=mock_env,
-        logger=mock_logger
-    )
-    mock_api.call_api = AsyncMock(return_value={'result': 'real_success'})
+    api.call_api = AsyncMock(return_value={'result': 'real_success'})
 
-    result = await mock_api.get_account_balance()
+    result = await api.get_account_balance()
 
     assert result == {'result': 'real_success'}
-    mock_logger.info.assert_called_with("실전투자 계좌 잔고 조회 시도...")
+    api._logger.info.assert_called_with("실전투자 계좌 잔고 조회 시도...")
 
 @pytest.mark.asyncio
 async def test_get_real_account_balance_without_dash():
-    mock_config = {
-        'tr_ids': {'account': {'inquire_balance_real': 'real_tr_id'}},
-        'custtype': 'P',
-        'stock_account_number': '12345678'
-    }
+    api = get_api()
+    api._env.is_paper_trading = False
 
-    mock_env = MagicMock()
-    mock_env.is_paper_trading = False
-    mock_logger = MagicMock()
-    mock_api = KoreaInvestApiAccount(
-        env=mock_env,
-        logger=mock_logger
-    )
-    mock_api.call_api = AsyncMock(return_value={'result': 'real_success_without_dash'})
+    api.call_api = AsyncMock(return_value={'result': 'real_success_without_dash'})
 
-    result = await mock_api.get_account_balance()
+    result = await api.get_account_balance()
 
     assert result == {'result': 'real_success_without_dash'}
-    mock_logger.info.assert_called_with("실전투자 계좌 잔고 조회 시도...")
+    api._logger.info.assert_called_with("실전투자 계좌 잔고 조회 시도...")
