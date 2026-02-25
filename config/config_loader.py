ebc649c7b3d10b0e49780491474ab82d0be6fa06
Kyungsoo <ksna703@gmail.com>
2026-02-25 17:19:49 +0900

    로그 파일 분리기능/ 오래된 로그파일 삭제 기능 추가

--- config/config_loader.py
+++ config/config_loader.py
@@ -2,7 +2,6 @@
 import yaml
 import os
 import json
-from functools import lru_cache
 
 # config.yaml 및 tr_ids_config.yaml 파일 경로 설정
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -19,7 +18,7 @@ KIS_CONFIG_PATH = os.path.join(BASE_DIR, 'kis_config.yaml')
 #         return yaml.safe_load(f)
 #
 #
-@lru_cache(maxsize=None)
+
 def load_configs() -> dict:
     main_config_data = load_config(MAIN_CONFIG_PATH)
     tr_ids_data = load_config(TR_IDS_CONFIG_PATH)
@@ -32,7 +31,7 @@ def load_configs() -> dict:
 
     return config_data
 
-@lru_cache(maxsize=None)
+
 def load_config(file_path):
     """지정된 경로에서 YAML 또는 JSON 설정 파일을 로드합니다."""
     try:
